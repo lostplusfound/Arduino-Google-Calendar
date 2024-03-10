90813da082f02c270642aaf1986bf3517a69f365
meebplayzs <57611739+meebplayzs@users.noreply.github.com>
2024-03-10 16:04:53 -0700

    Update login.ino

--- src/login.ino
+++ src/login.ino
@@ -1,49 +1,33 @@
 void login() {
   JSONVar token;
-  if(EEPROM.read(0) == 127) {
-    Serial.println(F("Found refresh in EEPROM"));
-    refresh_token = readStr(1);
-    Serial.println(refresh_token);
+  initTFT();
+  refresh_token = prefs.getString("refresh_token", "");
+  if(refresh_token.length() > 0) {
     access_token = refresh(refresh_token);
-    Serial.println(access_token);
   } 
-  if(access_token.length() == 0 || EEPROM.read(0) != 127) {
-    Serial.println(F("Invalid refresh in EEPROM"));
+  if(access_token.length() == 0) {
     tft.fillScreen(TFT_WHITE);
     tft.setCursor(0, 0, 2);
-    tft.setTextSize(3);
-    tft.println(F("Go to to log in! Press anywhere once logged in."));
+    tft.setTextSize(4);
+    tft.println(F("Not logged in!"));
+    tft.setTextSize(2);
+    tft.println(F("1) Go to https://tinyurl.com/arduinocalendar and log in with your Google Account"));
+    tft.println(F("2) Press anywhere once logged in"));
     while(digitalRead(D3) == 1) {
       yield();
     }
     token = JSON.parse(getToken());
     if(token.hasOwnProperty("access_token")) {
       access_token = String(token["access_token"]);
-      Serial.print(F("Access token: "));
-      Serial.println(access_token);
     }
     if(token.hasOwnProperty("refresh_token")) {
       refresh_token = String(token["refresh_token"]);
-      Serial.print(F("Refresh token: "));
-      Serial.println(refresh_token);
     }
-    EEPROM.write(0, 127);
-    putStr(1, refresh_token);
-    Serial.println(F("Saved refresh in EEPROM"));
+    prefs.putString("refresh_token", refresh_token);
   }
   tft.fillScreen(TFT_WHITE);
   tft.setCursor(0, 0, 2);
   tft.setTextSize(3);
   tft.println(F("Logged in!"));
-  timeNow = getTimeUnix();
-  while(timeNow == -1) {
-    delay(100);
-    timeNow = getTimeUnix();
-  }
-  rawOffset = getRawOffset();
-  while(rawOffset == -1) {
-    delay(100);
-    rawOffset = getRawOffset();
-  }
-  timeNow += rawOffset;
+  refreshTime();
 }
