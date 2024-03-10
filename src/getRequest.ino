3a9928486ddaf7d498a210647a51dd1b83663a96
meebplayzs <57611739+meebplayzs@users.noreply.github.com>
2024-03-10 16:04:20 -0700

    Update getRequest.ino

--- src/getRequest.ino
+++ src/getRequest.ino
@@ -2,7 +2,7 @@ String getRequest(String &access_token, String &time) {
   HTTPClient http;
   WiFiClientSecure client;
   client.setInsecure();
-  http.begin(client, String(REQUEST_LINK) + "&timeMin=" + time + "&access_token=" + access_token);
+  http.begin(client, REQUEST_LINK + "&timeMin=" + time + "&access_token=" + access_token);
   int code = http.GET();
   if(code == 200) {
     return http.getString();
