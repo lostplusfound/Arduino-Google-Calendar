e407fe02a9909087a3a38bd45b58de297ca2b713
meebplayzs <57611739+meebplayzs@users.noreply.github.com>
2024-06-11 17:33:34 -0700

    Update calendar.ino

--- src/calendar.ino
+++ src/calendar.ino
@@ -1,37 +1,23 @@
+#include <GoogleCalendarClient.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <Arduino_JSON.h>
 #include <WiFiClientSecureBearSSL.h>
 #include <SPI.h>
 #include <TFT_eSPI.h>
-#include <time.h>
 #include <WiFiManager.h>
-#include <Preferences.h>
-const String AUTHENTICATION_LINK = "";
-const String API_KEY = "";
-const String REQUEST_LINK = "https://www.googleapis.com/calendar/v3/calendars/primary/events?key=" + API_KEY;
-const String GEOLOCATION_LINK = "https://worldtimeapi.org/api/ip";
-const String REFRESH_LINK = "https://oauth2.googleapis.com/token";
-const String CLIENT_ID = "";
-const String CLIENT_SECRET = "";
+#include <ezTime.h>
+
+String AUTH_URI = "YOUR AUTH URI";
+String API_KEY = "YOUR API KEY";
+String CLIENT_ID = "YOUR CLIENT ID";
+String CLIENT_SECRET = "YOUR CLIENT SECRET";
+String GEOIP_URI = "https://worldtimeapi.org/api/ip";
 TFT_eSPI tft = TFT_eSPI();
+GoogleCalendarClient calendar(AUTH_URI, API_KEY, CLIENT_ID, CLIENT_SECRET);
 WiFiManager wifiManager;
-Preferences prefs;
-String access_token;
-String refresh_token;
-String timestamp;
-String date;
-time_t timeNow;
-int rawOffset;
-String getToken();
-String getRequest(String &access_token);
-String toRFC(time_t timeNow, int offset);
-String getTime();
-String toFormatString(time_t timeNow);
-time_t getTimeUnix();
-int getRawOffset();
-void refreshTime();
-String refresh(String &refresh_token);
+Timezone myTZ;
 void login();
+String getTimeZone();
 void configModeCallback(WiFiManager *myWiFiManager);
 void initTFT();
