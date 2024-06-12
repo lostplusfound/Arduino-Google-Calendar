5f939187bb5aed7b67e458172f7d9523148e2249
meebplayzs <57611739+meebplayzs@users.noreply.github.com>
2024-06-11 17:33:53 -0700

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,62 +1,87 @@
 unsigned long prevMillis;
-int count = 0;
 bool backlight = HIGH;
 void setup() {
   pinMode(D2, OUTPUT);
   pinMode(D3, INPUT);
   digitalWrite(D2, HIGH);
-  access_token.reserve(256);
-  refresh_token.reserve(128);
-  timestamp.reserve(32);
-  date.reserve(32);
-  Serial.begin(115200);
-  prefs.begin("calendar");
   initTFT();
   wifiManager.setDebugOutput(false);
   wifiManager.setAPCallback(configModeCallback);
   wifiManager.autoConnect("Calendar", "calendar123");
+  waitForSync();
+  myTZ.setLocation(getTimeZone());
   login();
   prevMillis = millis() + 30000;
 }
 
 void loop() {
   unsigned long nowMillis = millis();
-  JSONVar token;
   if(nowMillis - prevMillis > 30000) { // refresh every 30s and print to screen
     initTFT();
-    count++;
-    timeNow += 30;
-    timestamp = toRFC(timeNow, rawOffset);
-    token = JSON.parse(getRequest(access_token, timestamp));
-    while(!token.hasOwnProperty("items")) {
-      login();
-      token = JSON.parse(getRequest(access_token, timestamp));
-    }
+    String dateTimeNow = myTZ.dateTime(RFC3339);
+    String dateNow = dateTimeNow.substring(0, 10);
+    if(!calendar.refreshEvents(3, dateTimeNow)) login();
+    JSONVar events = calendar.events;
     tft.fillScreen(TFT_WHITE);
     tft.setCursor(0, 0, 2);
     tft.setTextSize(4);
-    tft.println(toFormatString(timeNow));
+    tft.println(myTZ.dateTime("H:i"));
+    tft.println(myTZ.dateTime("D, M j Y"));
     tft.setTextSize(3);
     tft.println(F("Today's events: "));
-    for(int i = 0; i < token["items"].length(); i++) {
-      date = String((const char *) token["items"][i]["start"]["dateTime"]);
-      if(timestamp.substring(0, 10) == date.substring(0, 10)) {
-        tft.print((const char *) token["items"][i]["summary"]);
+    for(int i = 0; i < events["items"].length(); i++) {
+      String startDateTime = String((const char *) events["items"][i]["start"]["dateTime"]);
+      String startDate = startDateTime.substring(0, 10);
+      if(dateNow == startDate) {
+        String startTime = startDateTime.substring(11, 16);
+        tft.print((const char *) events["items"][i]["summary"]);
         tft.print(F(" at: "));
-        tft.println(date.substring(11, 16));
+        tft.println(startTime);
       }
     }
     prevMillis = nowMillis;
-  }
-  if(count >= 20) { // refresh time every 10 min
-    refreshTime();
-    count = 0;
   }
   if(digitalRead(D3) == 0) { // toggle backlight if touchscreen was pressed
     backlight = !backlight;
     digitalWrite(D2, backlight);
     delay(500);
   }
+}
+
+void login() {
+  initTFT();
+  if(WiFi.status() != WL_CONNECTED) {
+    wifiManager.autoConnect("Calendar", "calendar123");
+  } else if(!calendar.refreshAccessToken()) {
+    tft.fillScreen(TFT_WHITE);
+    tft.setCursor(0, 0, 2);
+    tft.setTextSize(4);
+    tft.println(F("Not logged in!"));
+    tft.setTextSize(2);
+    tft.println(F("1) Go to https://tinyurl.com/arduinocalendar and log in with your Google Account"));
+    tft.println(F("2) Press anywhere once logged in"));
+    while(digitalRead(D3) == 1) {
+      yield();
+    }
+    calendar.getAccessToken();
+  }
+  tft.fillScreen(TFT_WHITE);
+  tft.setCursor(0, 0, 2);
+  tft.setTextSize(3);
+  tft.println(F("Logged in!"));
+}
+
+String getTimeZone() {
+  HTTPClient http;
+  WiFiClientSecure client;
+  client.setInsecure();
+  http.begin(client, GEOIP_URI);
+  int code = http.GET();
+  JSONVar time = JSON.parse(http.getString());
+  if(code > 0) {
+    if(time.hasOwnProperty("timezone")) return String(time["timezone"]);
+  }
+  return "";
 }
 
 void configModeCallback (WiFiManager *myWiFiManager) {
